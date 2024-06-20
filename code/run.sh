d525a5831922e90dcd4c090e88ac7ff3ad5ff614
AlexisG <alexisgonin68@gmail.com>
2024-06-20 15:39:28 +0200

    run update

--- code/run.sh
+++ code/run.sh
@@ -1,4 +1,4 @@
-rm -r ../home/feelpp/feelppdb/maxwell_particle
+rm -r ~/feelppdb/maxwell_particle
 rm -r ./maxwell_particle
 #mpirun -np 4 feelpp_toolbox_coefficientformpdes --config-file=maxwell_particle.cfg --hostfile hostfile
 mpirun --hostfile hostfile feelpp_toolbox_coefficientformpdes --config-file=maxwell_particle.cfg
